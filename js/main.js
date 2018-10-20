baf1cd0dd15308a29bfdd1a368894e6baddd4f39
Kristen Harlin <kristenharlin@Kristens-MacBook-Pro.local>
2018-10-20 19:27:27 -0400

    Add variables to select nav buttons

--- js/main.js
+++ js/main.js
@@ -1,1 +1,6 @@
 // homework 9 main js
+
+// background to dark gray
+
+var navButton = document.querySelectorAll('#desktop-menu ul li a')[3];
+var navButton2 = document.querySelectorAll('#desktop-menu ul li a')[4];
