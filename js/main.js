2f9350adb2402017412bde35f59b162dd1514443
Kristen Harlin <kristenharlin@Kristens-MacBook-Pro.local>
2018-10-20 20:46:47 -0400

    Add locationList variable

--- js/main.js
+++ js/main.js
@@ -7,9 +7,12 @@ var navButton2 = document.querySelectorAll('#desktop-menu ul li a')[4];
 
 var hoverNav = function (){
   this.classList.toggle('grayout');
-}
+};
 
 navButton.onmouseenter = hoverNav;
 navButton.onmouseleave = hoverNav;
 navButton2.onmouseenter = hoverNav;
 navButton2.onmouseleave = hoverNav;
+
+// part ii - showing the locations on click
+var locationList = document.getElementById('locations');
